a89369510b05a5f7cb6150d92fb25664366cc94b
Juha Törnroos <juha.tornroos@csc.fi>
2017-11-21 14:50:35 +0100

    Upgrade elixir_core library implementation to Spring Boot 1.3.0

--- elixir_core/src/main/java/org/ega_archive/elixircore/config/Cache.java
+++ elixir_core/src/main/java/org/ega_archive/elixircore/config/Cache.java
@@ -7,6 +7,8 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.guava.GuavaCacheManager;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.CacheResolver;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.cache.interceptor.SimpleKeyGenerator;
 import org.springframework.context.annotation.Bean;
@@ -42,6 +44,19 @@ public class Cache implements CachingConfigurer {
   @Override
   public KeyGenerator keyGenerator() {
     return new SimpleKeyGenerator();
+  }
+
+  
+  @Override
+  public CacheResolver cacheResolver() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public CacheErrorHandler errorHandler() {
+    // TODO Auto-generated method stub
+    return null;
   }
 
 }
