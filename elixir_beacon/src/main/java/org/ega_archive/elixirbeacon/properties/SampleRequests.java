9db4d56df10b69d0d728bf9f977b11aeacde7c3b
sdelatorrep <sabela.delatorrep@gmail.com>
2018-04-25 16:30:43 +0200

    Change sample queries.

--- elixir_beacon/src/main/java/org/ega_archive/elixirbeacon/properties/SampleRequests.java
+++ elixir_beacon/src/main/java/org/ega_archive/elixirbeacon/properties/SampleRequests.java
@@ -19,39 +19,79 @@ public class SampleRequests {
   @NotNull
   private String assemblyId1;
   
-  @NotNull
-  private String assemblyId2;
+  private Integer start1;
   
-  @NotNull
-  private String assemblyId3;
+  private Integer startMin1;
   
-  @NotNull
-  private Integer position1;
+  private Integer startMax1;
+
+  private Integer end1;
   
-  @NotNull
-  private Integer position2;
+  private Integer endMin1;
   
-  @NotNull
-  private Integer position3;
+  private Integer endMax1;
   
   @NotNull
   private String referenceName1;
   
   @NotNull
+  private String referenceBases1;
+  
+  private List<String> datasetIds1;
+  
+  private String alternateBases1;
+  
+  @NotNull
+  private String assemblyId2;
+  
+  private Integer start2;
+  
+  private Integer startMin2;
+  
+  private Integer startMax2;
+  
+  private Integer end2;
+  
+  private Integer endMin2;
+  
+  private Integer endMax2;
+  
+  @NotNull
   private String referenceName2;
+  
+  @NotNull
+  private String referenceBases2;
+  
+  private String variantType2;
+  
+  private List<String> datasetIds2;
+  
+  private String alternateBases2;
+  
+  @NotNull
+  private String assemblyId3;
+  
+  private Integer start3;
+  
+  private Integer startMin3;
+  
+  private Integer startMax3;
+  
+  private Integer end3;
+  
+  private Integer endMin3;
+  
+  private Integer endMax3;
   
   @NotNull
   private String referenceName3;
   
-  private List<String> datasetIds1;
+  @NotNull
+  private String referenceBases3;
   
-  private List<String> datasetIds2;
+  private String variantType3;
   
   private List<String> datasetIds3;
-  
-  private String alternateBases1;
-  
-  private String alternateBases2;
   
   private String alternateBases3;
   
